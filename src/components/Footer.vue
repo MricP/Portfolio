cbaef3edf932655d7333830112e3a0ce4d01af49
Pirrera Emric <emric.pirrera@etu.univ-lyon1.fr>
2025-04-18 13:48:41 +0200

    feat() -> footer layout

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,10 @@
 <template>
-    <div class="w-100% h-50 bg-[#EAEAEA] flex-col justify-center align-center">
-        <div class="flex align-center justify-between">
-            <img class="h-7 w-7" src="../assets/images/github.svg" alt="logo github">
-            <img src="" alt="">
-            <img src="" alt="">
-        </div>
-        
-        <p>© 2025 by Emric Pirrera</p>
+    <div class="w-100% h-50 bg-[#EAEAEA] flex flex-col justify-center align-center gap-10">
+            <div class="flex align-center justify-center gap-5">
+                <img class="h-7 w-7" src="../assets/images/github.svg" alt="logo github">
+                <img class="h-7 w-7" src="../assets/images/linkedin.svg" alt="logo linkedin">
+                <!-- <img class="h-7 w-7" src="" alt="logo mail"> -->
+            </div>
+            <p class="text-center">© 2025 by Emric Pirrera</p>
     </div>
 </template>
