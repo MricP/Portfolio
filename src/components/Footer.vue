d019af5e030ae95fd05b99bdebc80dcd7a69c9b5
Pirrera Emric <emric.pirrera@etu.univ-lyon1.fr>
2025-04-18 13:55:59 +0200

    deploy() -> footer (mail svg missing but all back work)

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="w-100% h-50 bg-[#EAEAEA] flex flex-col justify-center align-center gap-10">
             <div class="flex align-center justify-center gap-5">
-                <img class="h-7 w-7" src="../assets/images/github.svg" alt="logo github">
-                <img class="h-7 w-7" src="../assets/images/linkedin.svg" alt="logo linkedin">
-                <!-- <img class="h-7 w-7" src="" alt="logo mail"> -->
+                <a href="https://github.com/MricP"><img class="h-7 w-7" src="../assets/images/github.svg" alt="logo github"></a>
+                <a href="https://www.linkedin.com/in/emric-pirrera-4925aa296/" target="_blank"><img class="h-7 w-7" src="../assets/images/linkedin.svg" alt="logo linkedin"></a>
+                <a href="https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcRwRfDZRNtCSVBRwttfVsXBmnMhwKbGmLzlnjHZtnwmqnpDCDdQBGZPWkWHFQhTwmkFQxsWP"><img class="h-7 w-7" src="../assets/images/github.svg" alt="logo github"></a>
             </div>
             <p class="text-center">© 2025 by Emric Pirrera</p>
     </div>
