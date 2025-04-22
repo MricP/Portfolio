a48b3ffe3c6f88a0c54162d7bc34a1b0ad40053f
Pirrera Emric <emric.pirrera@etu.univ-lyon1.fr>
2025-04-22 13:30:08 +0200

    responsive() -> init home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,14 +6,14 @@ import ProjectContainer from '../components/ProjectContainer.vue';
 
 <template>
     <main>
-        <div class="flex justify-center gap-15 m-35">
-            <div class="w-125 h-90 bg-[#D9D9D9] inset-shadow-sm inset-shadow-[#00000025]"></div>
+        <div class="flex justify-center gap-15 lg:m-35  m-35 sm:m-5 sm:mt-30 flex-col lg:flex-row ">
+            <div class="lg:w-125 md:w-125 h-90 bg-[#D9D9D9] inset-shadow-sm inset-shadow-[#00000025]"></div>
             <div class="flex flex-col space-y-20 gap-5 items-left w-125 h-90">
-                <div class="flex flex-col space-y-5 items-left">
-                    <h1 class="text-5xl bg-gradient-to-r from-[#A9A9FF] to-[#D9D9D9] bg-clip-text text-transparent shadow-inner-text">Emric Pirrera</h1>
-                    <h2 class="text-2xl text-transparent bg-gradient-to-r from-[#D9D9D9] to-[#A9A9FF] bg-clip-text [filter:drop-shadow(1px_1px_1px_rgba(0,0,0,0.3))]">Software Developer</h2>
+                <div class="flex flex-col space-y-5 items-left ">
+                    <h1 class="lg:md:text-5xl text-3xl bg-gradient-to-r from-[#A9A9FF] to-[#D9D9D9] bg-clip-text text-transparent shadow-inner-text">Emric Pirrera</h1>
+                    <h2 class="lg:md:text-2xl text-xl text-transparent bg-gradient-to-r from-[#D9D9D9] to-[#A9A9FF] bg-clip-text [filter:drop-shadow(1px_1px_1px_rgba(0,0,0,0.3))]">Software Developer</h2>
 
-                    <p>Tout ce que vous pouvez imaginer est probablement faisable, il vous suffit de l'imaginer et d'y travailler. (Larry Page)</p>
+                    <p class="sm:text-lg">Tout ce que vous pouvez imaginer est probablement faisable, il vous suffit de l'imaginer et d'y travailler. (Larry Page)</p>
 
                 </div>
                 <div class="flex flex-col space-y-5 items-left">
