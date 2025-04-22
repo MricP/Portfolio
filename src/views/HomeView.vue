cdb67a790b5aae79d043e78c55f8f7a414c7a656
Pirrera Emric <emric.pirrera@etu.univ-lyon1.fr>
2025-04-22 13:52:02 +0200

    modification margin top

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,18 +5,18 @@ import ProjectContainer from '../components/ProjectContainer.vue';
 </script>
 
 <template>
-    <main>
-        <div class="flex justify-center gap-15 lg:m-35  m-35 sm:m-5 sm:mt-30 flex-col lg:flex-row ">
-            <div class="lg:w-125 md:w-125 h-90 bg-[#D9D9D9] inset-shadow-sm inset-shadow-[#00000025]"></div>
-            <div class="flex flex-col space-y-20 gap-5 items-left w-125 h-90">
-                <div class="flex flex-col space-y-5 items-left ">
+    <main class="w-100%">
+        <div class="flex flex-col md:flex-row justify-center mt-35 ml-4 sm:mt-15 sm:ml-5sm:mr-5 md:ml-6 gap-4">
+            <div class="w-full sm:w-1/2 lg:w-[500px] h-90 bg-[#D9D9D9] shadow-inner"></div>        
+                <div class="flex flex-col space-y-20 gap-5 items-left lg:md:w-100% w-90">
+                <div class="flex flex-col space-y-5 items-left lg:md:w-100% w-90 ">
                     <h1 class="lg:md:text-5xl text-3xl bg-gradient-to-r from-[#A9A9FF] to-[#D9D9D9] bg-clip-text text-transparent shadow-inner-text">Emric Pirrera</h1>
                     <h2 class="lg:md:text-2xl text-xl text-transparent bg-gradient-to-r from-[#D9D9D9] to-[#A9A9FF] bg-clip-text [filter:drop-shadow(1px_1px_1px_rgba(0,0,0,0.3))]">Software Developer</h2>
 
-                    <p class="sm:text-lg">Tout ce que vous pouvez imaginer est probablement faisable, il vous suffit de l'imaginer et d'y travailler. (Larry Page)</p>
+                    <p class="lg:md:text-l lg:md:w-100% text-sm w-90">Tout ce que vous pouvez imaginer est probablement faisable, il vous suffit de l'imaginer et d'y travailler. (Larry Page)</p>
 
                 </div>
-                <div class="flex flex-col space-y-5 items-left">
+                <div class="flex flex-col justify-center space-y-5 items-left lg:md:w-100% w-90">
                     <button class="w-30 h-10 border-solid border-2 border-black hover:bg-[#E9E9E9]">Contacter</button>
                     <a href="#" class="relative inline-block text-black transition-colors duration-300 hover:text-black hover:underline underline-offset-4">En savoir plus</a>
                 </div>
